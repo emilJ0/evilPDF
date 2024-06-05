1db5effca16198a39c7cd4a6e497f260cf399ef9
emilJ0 <133218806+emilJ0@users.noreply.github.com>
2024-06-06 01:26:06 +0200

    Delete KEL directory

--- KEL/script/startsession.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ip=$1 # nimmt übergebenes Argument auf und speichert es in der Variable "ip"
-DSTIP="$ip" msfconsole -r msf/sessions.rc # übergibt mithilfe von DSTIP "ip" an das Resource Script und startet dieses
