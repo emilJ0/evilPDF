a1c0c2f72d65d218b984c70f57eea69ed9c0285c
emilJ0 <133218806+emilJ0@users.noreply.github.com>
2024-06-06 15:44:40 +0200

    Delete explanations/save.py

--- explanations/save.py
+++
@@ -1,4 +0,0 @@
-speicherDatei = open("explanations/speicherDatei.txt", "a")
-speicherDatei.write("Neuer Eintrag in Datei")
-speicherDatei = open("explanations/speicherDatei.txt", "r")
-print(speicherDatei.read())
